7ee15615ca1b3fd36e1ee0a58be0a315f383dd42
isixe <isixe@outlook.com>
2023-05-30 01:24:58 +0800

    :sparkles: feature(roomController): 房间的分页模糊查询

--- src/main/java/com/too/trip/controller/RoomController.java
+++ src/main/java/com/too/trip/controller/RoomController.java
@@ -1,8 +1,13 @@
 package com.too.trip.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.too.trip.entity.R;
 import com.too.trip.entity.Room;
 import com.too.trip.entity.Scenic;
+import com.too.trip.entity.User;
 import com.too.trip.service.RoomService;
 import com.too.trip.service.impl.RoomServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +29,7 @@ import java.util.UUID;
  * @author isixe
  * @since 2023-05-24
  */
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/room")
 public class RoomController {
@@ -35,11 +40,17 @@ public class RoomController {
     @Autowired
     private ResourceLoader resourceLoader;
 
-
-
-    @PostMapping("insert")
-    public R insertScenic(@RequestBody @RequestParam("file") MultipartFile file, Room room) throws IOException {
-        if(file.isEmpty()){
+    /**
+     * 房间的增加
+     *
+     * @param file
+     * @param room
+     * @return 房间实体
+     * @throws IOException
+     */
+    @PostMapping
+    public R<Room> insertRoom(@RequestBody @RequestParam("file") MultipartFile file, Room room) throws IOException {
+        if (file.isEmpty()) {
             return new R(400, "文件不能为空");
         }
 
@@ -54,7 +65,7 @@ public class RoomController {
         Resource resource = resourceLoader.getResource(path);
         File dir = resource.getFile();
 
-        File destFile  = new File(dir, fileName);
+        File destFile = new File(dir, fileName);
         file.transferTo(destFile);
 
         // 设置hotel的hotel_img属性
@@ -62,7 +73,7 @@ public class RoomController {
         room.setRoomImg(fileName);
         //boolean isSaveSuccess = hotelService.save(scenic);
         boolean isSaveSuccess = roomService.save(room);
-        if(!isSaveSuccess){
+        if (!isSaveSuccess) {
             return new R(400, "插入失败");
         }
         return new R();
@@ -95,41 +106,20 @@ public class RoomController {
         return new R<>();
     }
 
-    /**
-     * 根据酒店 ID 查询所有可用的某种房间类型列表
-     *
-     * @param hotelId 酒店 ID
-     * @return 某种房间类型列表
-     */
-    @GetMapping("/type")
-    public List<Room> getRoomTypeByHotelId(@RequestParam("hotelId") Integer hotelId) {
-        return roomService.selectRoomTypeByHotelId(hotelId);
+    @GetMapping("/page/{start}/{size}/{field}/{keyword}")
+    public R<Page> getRoom(@PathVariable("start") Integer pages,
+                           @PathVariable("size") Integer pageSize,
+                           @PathVariable("field") String field,
+                           @PathVariable("keyword") String keyword) {
+
+        //页码数小于0 设置为0
+        if (pages == null || pages < 0) {
+            pages = 0;
+        }
+
+        Page<Room> page = roomService.searchPages(pages, pageSize, field, keyword);
+
+        return new R<>(page);
     }
 
-
-    /**
-     * 更新指定 ID 的房间信息
-     *
-     * @param roomId 要更新的房间 ID
-     * @param room   新的房间信息
-     */
-    @PutMapping("{roomId}")
-    public void updateRoom(@PathVariable("roomId") Integer roomId, @RequestBody Room room) {
-        // 设置要更新的房间的 ID
-        room.setRoomId(roomId);
-        // 调用底层服务来更新房间信息
-        roomService.updateRoom(room);
-    }
-
-    /**
-     * 根据酒店 ID 和房间类型查询房间列表
-     *
-     * @param hotelId  酒店 ID
-     * @param roomtype 房间类型
-     * @return 房间列表
-     */
-    @GetMapping
-    public List<Room> getRooms(@RequestParam("hotelId") Integer hotelId, @RequestParam("roomtype") String roomtype) {
-        return roomService.selectRoomByHotelIdAndRoomType(hotelId, roomtype);
-    }
 }
--- src/main/java/com/too/trip/service/impl/RoomServiceImpl.java
+++ src/main/java/com/too/trip/service/impl/RoomServiceImpl.java
@@ -1,6 +1,8 @@
 package com.too.trip.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.too.trip.entity.Hotel;
 import com.too.trip.entity.Room;
 import com.too.trip.mapper.RoomMapper;
 import com.too.trip.service.RoomService;
@@ -12,7 +14,7 @@ import java.util.List;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author isixe
@@ -20,41 +22,22 @@ import java.util.List;
  */
 @Service
 public class RoomServiceImpl extends ServiceImpl<RoomMapper, Room> implements RoomService {
-    @Autowired RoomMapper roomMapper;
-//查找房间类型通过酒店id
-    @Override
-    public List<Room> selectRoomTypeByHotelId(Integer hotelId) {
-        return roomMapper.selectRoomTypeByHotelId(hotelId);
-    }
-//添加房间
-    @Override
-    public void insertRoom(Room room) {
-        roomMapper.insert(room);
-    }
-//查找房间通过房间id
+    @Autowired
+    RoomMapper roomMapper;
+
     @Override
     public Room selectRoomById(Integer roomId) {
         return roomMapper.selectById(roomId);
     }
-//修改房间
+
     @Override
-    public void updateRoom(Room room) {
-        roomMapper.updateById(room);
+    public Page<Room> searchPages(Integer pages, Integer pageSize, String filed, String keyword) {
+        Page<Room> page = new Page<>(pages, pageSize);
+        roomMapper.selectPages(page, pageSize, filed, keyword);
+        return page;
     }
-//删除房间
-    @Override
-    public void deleteRoomById(Integer roomId) {
-        roomMapper.deleteById(roomId);
-    }
-//查找房间通过酒店id和房间id
-    @Override
-    public List<Room> selectRoomByHotelIdAndRoomType(Integer hotelId, String roomtype) {
-        return roomMapper.selectList(
-                new QueryWrapper<Room>()
-                        .eq("h_id", hotelId)
-                        .eq("room_type", roomtype)
-        );
-    }
+
+
 }
 
 
